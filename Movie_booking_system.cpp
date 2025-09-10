14abc3ba340001a6e8bfd5384225bdfd3225bb8d
RudraSingh018911 <allenite76@gmail.com>
2025-09-11 00:56:17 +0530

    Update Movie_booking_system.cpp

--- Movie_booking_system.cpp
+++ Movie_booking_system.cpp
@@ -1,1 +1,23 @@
-
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+class movie {
+private:
+    string title;
+    string genre;
+    int duration;
+public:
+    movie(string title, string genre, int duration){
+        this->title = title;
+        this->genre = genre;
+        this->duration = duration;
+    }
+    void displayDetails(int index) {
+        cout << index << ". " << title << " | Genre: " << genre 
+             << " | Duration: " << duration << " mins" << endl;
+    }
+    string gettitle(){
+         return title;
+         }
+};
